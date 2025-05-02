50314f072c67637a81ddc5d4e2332309ca7f50a9
AMAN7040 <amansingh79620@gmail.com>
2025-05-02 18:48:27 +0530

    chore: install and configure Tailwind CSS v4  for initial styling setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
-import './App.css'
+import "./App.css";
 
 function App() {
-
-
   return (
     <>
-      <p>HELOOOO</p>
+      <p className="text-red-600">HELOOOO</p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
