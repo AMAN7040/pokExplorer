66e3b2f5c837971614b95408dfe0a3cc4d18ad70
AMAN7040 <amansingh79620@gmail.com>
2025-05-02 18:53:14 +0530

    chore: install react router and set up routing entry point

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 
 function App() {
   return (
-    <>
-      <p className="text-red-600">HELOOOO</p>
-    </>
+    <Routes>
+      <Route path="/" element={<div className="text-red-600">HELLOO</div>} />
+    </Routes>
   );
 }
 
